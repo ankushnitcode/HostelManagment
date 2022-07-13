c9ec309fd83551c74af3c7c313af49db39fc53a4
Vishal <balharavishu@gmail.com>
2022-07-13 15:58:39 +0530

    Return with PostMapping

--- src/main/java/com/example/BedManagement/Controllers/StudentRegistrationAndRequestController.java
+++ src/main/java/com/example/BedManagement/Controllers/StudentRegistrationAndRequestController.java
@@ -4,19 +4,18 @@ import com.example.BedManagement.Entity.Student;
 import com.example.BedManagement.Model.StudentInfo;
 import com.example.BedManagement.Repository.StudentRepository;
 import com.example.BedManagement.Services.StudentRegisterService;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
 
 @RestController
+@RequestMapping("/HostelSystem")
 public class StudentRegistrationAndRequestController {
     @Autowired
     StudentRepository studentRepository;
@@ -26,14 +25,29 @@ public class StudentRegistrationAndRequestController {
   String homePage(){
         return "Welcome";
     }
-    @PostMapping("/students")
-    public ResponseEntity<Object> createStudent(@RequestBody StudentInfo studentInfoBody) {
-        Student student = new Student();
-        student= studentRegisterService.createNewStudent(studentInfoBody);
-        Student savedUser = studentRepository.save(student);
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getStudentId())
-                .toUri();
 
-        return ResponseEntity.created(location).build();
+    @PostMapping("/students")
+    @ResponseBody
+    public StudentInfo createStudent(@RequestBody StudentInfo studentInfoBody)
+    {
+        Student student =
+                new Student();
+        student = studentRegisterService.createNewStudent(studentInfoBody);
+        Student savedStudent = studentRepository.save(student);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedStudent.getStudentId()).toUri();
+        ResponseEntity.created(location).build();
+        return studentInfoBody;
     }
+               //}
+
+
+//    @PostMapping("/students")
+//    public ResponseEntity<Object> createStudent(@RequestBody StudentInfo studentInfoBody) {
+//        Student student = new Student();
+//        student= studentRegisterService.createNewStudent(studentInfoBody);
+//        Student savedUser = studentRepository.save(student);
+//        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getStudentId())
+//                .toUri();
+//        return ResponseEntity.created(location).build();
+//    }
 }
