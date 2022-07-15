b921f3fed4c762b6ac41c1798b13119be20e4752
firsttoogo <ankushsainijnv001@gmail.com>
2022-07-15 16:39:32 +0530

    asssigning bed to student(giving them room)

--- src/main/java/com/example/BedManagement/Controllers/StudentRegistrationAndRequestController.java
+++ src/main/java/com/example/BedManagement/Controllers/StudentRegistrationAndRequestController.java
@@ -7,6 +7,7 @@ import com.example.BedManagement.Services.StudentRegisterService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -28,13 +29,25 @@ public class StudentRegistrationAndRequestController {
     }
     @PostMapping("/students")
     @ResponseBody
-    public StudentInfo createStudent(@Valid @RequestBody StudentInfo studentInfoBody) {
+    public ResponseEntity<Object> createStudent(@Valid @RequestBody StudentInfo studentInfoBody) {
         Student student =
                 new Student();
         student = studentRegisterService.createNewStudent(studentInfoBody);
         Student savedStudent = studentRepository.save(student);
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedStudent.getStudentId()).toUri();
-        ResponseEntity.created(location).build();
-        return studentInfoBody;
+       // URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedStudent.getStudentId()).toUri();
+       // ResponseEntity.created(location).build();
+        return new ResponseEntity<>(savedStudent,HttpStatus.CREATED);
     }
-}
+    @PostMapping("/request/{id}")
+    public ResponseEntity<Object> studentBedRequest(@PathVariable int id ){
+        if(!studentRepository.existsById(id)){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);}
+            else{
+                studentRegisterService.assigningBedToStudent(id);
+              return new   ResponseEntity<>(HttpStatus.OK);
+            }
+
+        }
+
+    }
+
